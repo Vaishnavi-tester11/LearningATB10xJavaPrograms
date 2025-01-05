b84f6540bbd2d68d4cd4afe6ea4536ded6b220ae
Vaishnavi-tester11 <vaishnavi.guvvala11@gmail.com>
2025-01-05 17:23:13 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab007_CommonMath_Exception.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab006_CommonMath {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2 * 2);
+        System.out.println(2/2);
+        System.out.println(2%2);
+        System.out.println(2-2);
+        System.out.println(3/2); // println always take integer part
+    }
+}
