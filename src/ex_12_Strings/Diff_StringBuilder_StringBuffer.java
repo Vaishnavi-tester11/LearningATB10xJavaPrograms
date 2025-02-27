9503818adb0eb92e134bb884e7e66472429344a5
Vaishnavi-tester11 <vaishnavi.guvvala11@gmail.com>
2025-02-27 14:44:45 +0530

    tasks

---
+++ src/ex_12_Strings/Diff_StringBuilder_StringBuffer.java
@@ -0,0 +1,25 @@
+package ex_12_Strings;
+
+public class Diff_StringBuilder_StringBuffer {
+    public static void main(String[] args) {
+
+        // we will String 90%
+        // StringBuilder 5-7%
+        // Thread local -> Multithreading
+
+        StringBuffer name=new StringBuffer("Vamsi");
+        name.append(" Reddy");
+        System.out.println(name);
+        name.reverse();
+        System.out.println(name);
+        name.reverse();
+        System.out.println(name);
+
+
+        StringBuilder sb=new StringBuilder(" Ahaana ");
+        sb.append("Reddy");
+        System.out.println();
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
