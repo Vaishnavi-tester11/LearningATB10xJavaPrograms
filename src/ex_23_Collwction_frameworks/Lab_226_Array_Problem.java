c5edf1428f9b264ecc5acf1ba1ca761e950b89f1
Vaishnavi-tester11 <vaishnavi.guvvala11@gmail.com>
2025-04-05 11:43:29 +0530

    tasks

---
+++ src/ex_23_Collwction_frameworks/Lab_226_Array_Problem.java
@@ -0,0 +1,15 @@
+package ex_23_Collwction_frameworks;
+
+public class Lab_226_Array_Problem {
+    public static void main(String[] args) {
+        Integer arr[]=new Integer[10];
+        arr[0]=1;
+        // fixed Size
+        // same data type
+        // doesn't grow automatically
+        // less elements -> wastage of memory. 100 size, 10 used, 90 wasted
+        // to over come these problems introduced Collection Framework
+
+
+    }
+}
